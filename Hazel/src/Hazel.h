dbb1ad01103947490bca0eb74811169441116e31
lryain <47129927@qq.com>
2023-12-11 21:21:56 +0800

    Hello world

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// for use by Hazel Application
+#include <stdio.h>
+#include "Hazel\Application.h"
+
+//--- Entry Point---------------------------
+#include "Hazel\EntryPoint.h"
+//--- Entry Point---------------------------
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,23 @@
+#include <Hazel.h>
+
+//namespace Hazel {
+//	__declspec(dllimport) void Print();
+//}
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+
+	~Sandbox()
+	{
+
+	}
+};
+
+Hazel::Application* Hazel::CreateApplication() {
+	return new Sandbox();
+}
