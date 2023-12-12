68494a1d691747ccbe08504069b022f3f948433f
lryain <47129927@qq.com>
2023-12-12 20:03:51 +0800

    迁徙到premake5

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // for use by Hazel Application
 #include <stdio.h>
 #include "Hazel\Application.h"
+#include "Hazel\Log.h"
 
 //--- Entry Point---------------------------
 #include "Hazel\EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,6 +7,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	//printf("Hello world!");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 4;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 
